2a49522bc2ca029b04d090175e5a3147c5bbc771
João Santos <pogrenyak@hotmail.com>
2020-08-01 00:13:30 +0100

    Basics

---
+++ basics/basics.js
@@ -0,0 +1,7 @@
+// Tudo com '//' é um comentário em linguagem natural
+// Os comentários são para ser lidos com atenção eles explicam o código.
+
+
+/* Capítulo 3: Tipos, Valores, Variáveis */
+// Números
+
