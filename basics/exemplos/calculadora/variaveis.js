73219db38140940c9148befa0b5318ea2691e27d
João Santos <pogrenyak@hotmail.com>
2020-08-12 00:37:49 +0100

    Alguns exemplos

---
+++ basics/exemplos/calculadora/variaveis.js
@@ -0,0 +1,38 @@
+// Linguagem JavaScript
+// Declaração das váriaveis lápis cada uma com a sua cor.
+var lapisVermelho = 'vermelho';
+var lapisPreto = 'preto';
+var lapisVerde = 'verde';
+var lapisLaranja = 'Laranja';
+var lapisCastanho = 'castanho';
+
+// Criando um vector para as cores
+// que preciso para pintar um desenho
+var caixaDeCores = ['vermelho', 'preto', 'castanho', 'laranja', 'verde']
+
+// Vetor de frutas para a minha dieta
+var frutas = ['manga', 'pera', 'macã', 'banana', 'ananas']
+
+// Vetor dos meus clubes favoritos
+var clubesFavoritos = ['Barcelona', 'Man Utd', 'B. Dortmund', '1º de Agosto']
+
+// Vetor das marcas de ténis que mais uso
+var marcasDeTenis = ['Nike', 'Adidas', 'Puma', 'Umbro']
+
+// Declarando a variável nome (1)
+let nome = 'Jeovanni'
+
+// Declarando a váriavel nome (2)
+var nome = 'Jo'
+
+/**
+ * O erro de síntaxe ocorre por tu já declaraste a variável
+ * "nome" antes e deste um escopo a ela. Quando tentas sobreescrever
+ * a mesma variavél provoca um erro de sintaxe. Isso não é permitido no 
+ * V8 que é o ambiente de execução do JavaScript no Node.js
+ */
+
+
+var msg = 'What\'s your name ?'
+var mgs1 = "What's your name bro ?"
+var msg2 = "Gostaria de te ver a sorrir 'velha burra'"
